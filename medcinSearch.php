5ab1a702cf3908457b35172c1fe246445cec0812
haythem <abdelheqr@gmail.com>
2024-05-23 14:46:27 +0100

    testing

---
+++ medcinSearch.php
@@ -0,0 +1,42 @@
+
+<!doctype html>
+<html>
+	<head>
+		<title>MA LISTE</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/Bootstrap.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="css/button.css">
+		<link rel="stylesheet" href="css/menu.css">
+		<link rel="stylesheet" href="css/logo.css">
+		<link rel="stylesheet" href="css/table.css">
+		<link rel="stylesheet" href="css/portail.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	</head>
+	
+	<body>
+		
+		
+<header class="header">
+
+<a href="#" class="logo"> <i class="fas fa-heartbeat"></i> medcare </a>
+
+<nav class="navbar">
+	<a href="#home">home</a>
+	<a href="#services">services</a>
+	<a href="#about">about</a>
+	<a href="#review">review</a>
+	<a href="deconnexion.php"> log out 
+		<i class="fas fa-sign-out-alt"></i>
+	</a>
+</nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+
+	</body>
+</html>
